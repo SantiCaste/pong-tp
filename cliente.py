a30a4d5476ea7259a1547229197382bf58e208d9
Donatella Fragassi <87719161+DFragassi@users.noreply.github.com>
2024-07-03 16:46:34 -0300

    reworkee todo

--- cliente.py
+++ cliente.py
@@ -4,8 +4,8 @@ import pygame
 import pygame.draw
 from pygame._sdl2 import Window
 from multiprocessing.connection import Listener,Client
+import sys
 
-# Dimensiones de la pantalla
 WIDTH, HEIGHT = 800, 600
 BALL_RADIUS = 10
 PADDLE_WIDTH, PADDLE_HEIGHT = 10, 100
@@ -13,10 +13,41 @@ WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 RED = (255, 0, 0)
 BLUE = (0, 0, 255)
-
+BUFFER = 1024
+XL=20
+XR=30
+NONE = -1
+ZERO = 0
+PLAYER_ONE = 0
+PLAYER_TWO = 1
+PORT = 5555
+IP = "127.0.0.1"
+BUFFER = 1024
+MIDDLE = 2
 DISPLAY_PLAYER_1 = (100, 200)
 DISPLAY_PLAYER_2 = (WIDTH + 200, 200)
 DISPLAY_CENTER = (WIDTH, 200)
+CLOCK = 60
+SIZE_FONT = 74
+CORDXL = 250
+CORDY= 10
+CORDXR = 510
+FAIL = 2
+WINNER_POS = 250
+MENU_TEXT_SIZE = 100
+MENU_TEXT_X=400
+MENU_TEXT_Y = 100
+PLAY_LAN_BUTTON_X = 400
+PLAY_LAN_BUTTON_Y = 250
+FONT_SIZE = 75
+QUIT_BUTTON_X = 400
+QUIT_BUTTON_Y = 400
+LOADING_TEXT_FONT_SIZE = 100
+LOADING_TEXT_X = 400
+LOADING_TEXT_Y = 300
+FAIL_TEXT_X = 400
+FAIL_TEXT_Y = 300
+FAIL_TEXT_FONT_SIZE = 25
 
 pygame.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -30,35 +61,33 @@ class Paddle_Client:
         self.port = port
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.client.connect((self.ip, self.port))
-        self.player = int(self.client.recv(1024).decode())
+        self.player = int(self.client.recv(BUFFER).decode())
         self.set_display()
-        #esta variable de entorno se usa para indicar en qué posición crear la pantalla
         window = Window.from_display_module()
-        window.position = (self.display_x,self.display_y) # Will place the window around the screen 
+        window.position = (self.display_x,self.display_y) 
         screen = pygame.display.set_mode((WIDTH, HEIGHT))
         pygame.display.set_caption("Pong Client")        
         self.screen = screen
         self.set_paddle()
         self.ball = Ball()
         self.running = True
-        self.score = [0, 0]
-        self.winner = -1
+        self.score = [ZERO, ZERO]
+        self.winner = NONE
 
-    #esta función sería para settear las pantallas una al lado de la otra, pero no pude hacerlo andar.
     def set_display(self): 
         print(self.player)   
-        if self.player == 0:
+        if self.player == PLAYER_ONE:
             self.display_x, self.display_y = DISPLAY_CENTER
         else:
             self.display_x, self.display_y = DISPLAY_CENTER
     
     def set_paddle(self):    
-        if self.player == 0:
-            self.paddleL = Paddle(20, BLUE)
-            self.paddleR = Paddle(WIDTH - 30, RED)
+        if self.player == PLAYER_ONE:
+            self.paddleL = Paddle(XL, BLUE)
+            self.paddleR = Paddle(WIDTH - XR, RED)
         else:
-            self.paddleL = Paddle(20, RED)
-            self.paddleR = Paddle(WIDTH - 30, BLUE)
+            self.paddleL = Paddle(XL, RED)
+            self.paddleR = Paddle(WIDTH - XR, BLUE)
         
     def start_game(self):
         threading.Thread(target=self.receive_data).start()
@@ -68,7 +97,7 @@ class Paddle_Client:
     def receive_data(self):
         while self.running:
             try:
-                data = self.client.recv(1024).decode()
+                data = self.client.recv(BUFFER).decode()
                 if data:
                     p1_y, p2_y, ball_x, ball_y, score1, score2, self.winner = map(int, data.split(','))
                     self.paddleL.update(p1_y)
@@ -79,65 +108,79 @@ class Paddle_Client:
                 continue
             
     def handle_key_events_lan(self):
+        self.check_quit_event()
+        self.handle_keyboard_input()
+
+    def check_quit_event(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                self.running = False
-                self.client.close()
-                pygame.quit()
-                quit()
+                self.quit_game()
 
+    def handle_keyboard_input(self):
         keys = pygame.key.get_pressed()
-        if keys[pygame.K_q]: #si apreta la 'q' se desconecta el client
-            self.running = False
-            self.client.send(str.encode('QUIT'))
+        if keys[pygame.K_q]:
+            self.send_quit_signal()
         elif keys[pygame.K_UP]:
-            self.client.send(str.encode('UP'))
+            self.send_move_signal('UP')
         elif keys[pygame.K_DOWN]:
-            self.client.send(str.encode('DOWN'))
+            self.send_move_signal('DOWN')
 
+    def quit_game(self):
+        self.running = False
+        self.client.close()
+        pygame.quit()
+        quit()
+
+    def send_quit_signal(self):
+        self.running = False
+        self.client.send(str.encode('QUIT'))
+
+    def send_move_signal(self, direction):
+        self.client.send(str.encode(direction))
 
     def handle_client(self):
         while self.running:
-            
-            #self.handle_key_events()
             self.handle_key_events_lan()
-            
-            self.screen.fill(BLACK)
-            self.ball.draw()
-            self.paddleL.draw()
-            self.paddleR.draw()
-
-            # Mostrar el marcador
-            font = pygame.font.Font(None, 74)
-            text = font.render(str(self.score[0]), 1, WHITE)
-            self.screen.blit(text, (250, 10))
-            text = font.render(str(self.score[1]), 1, WHITE)
-            self.screen.blit(text, (510, 10))
-            
-            # Mostrar el ganador (FALTA EL RESET DEL GUEGO)
-            if self.winner != -1:
-                #running = False #este es para que no siga procesando los inputs
-                font = pygame.font.Font(None, 74)
-                if self.winner == 0:
-                    winner_color = (RED if self.player == 0 else BLUE)
-                    text_to_show="Right player Wins"
-                    text_font = font.render(text_to_show, 1, winner_color)
-                else:
-                    winner_color = (RED if self.player == 1 else BLUE)
-                    text_to_show="Left player Wins"
-                    text_font = font.render(text_to_show, 1, winner_color)
-                
-                self.screen.blit(text_font, (250, 250))
-
+            self.update_screen()
             pygame.display.update()
-            self.clock.tick(60)    
+            self.clock.tick(CLOCK)
 
+    def update_screen(self):
+        self.screen.fill(BLACK)
+        self.draw_objects()
+        self.show_score()
+        self.show_winner()
 
-#####CLASS DEFINITION######################################
+    def draw_objects(self):
+        self.ball.draw()
+        self.paddleL.draw()
+        self.paddleR.draw()
+
+    def show_score(self):
+       
+        font = pygame.font.Font(None, SIZE_FONT)
+        score_left = font.render(str(self.score[0]), True, WHITE)
+        score_right = font.render(str(self.score[1]), True, WHITE)
+        self.screen.blit(score_left, (CORDXL, CORDY))
+        self.screen.blit(score_right, (CORDXR, CORDY))
+
+    def show_winner(self):
+        if self.winner != NONE:
+            font = pygame.font.Font(None, SIZE_FONT)
+            if self.winner == PLAYER_ONE:
+                winner_color = (RED if self.player == PLAYER_ONE else BLUE)
+                text_to_show = "Right player Wins"
+            else:
+                winner_color = (RED if self.player == PLAYER_TWO else BLUE)
+                text_to_show = "Left player Wins"
+            
+            winner_text = font.render(text_to_show, True, winner_color)
+            self.screen.blit(winner_text, (WINNER_POS, WINNER_POS))
+
 class Ball:
     def __init__(self):
-        self.x = WIDTH // 2
-        self.y = HEIGHT // 2
+        self.x = WIDTH // MIDDLE
+        self.y = HEIGHT // MIDDLE
 
     def update(self, x, y):
         self.x = x
@@ -149,7 +192,7 @@ class Ball:
 class Paddle:
     def __init__(self, x, color):
         self.x = x
-        self.y = HEIGHT // 2 - PADDLE_HEIGHT // 2
+        self.y = HEIGHT // MIDDLE - PADDLE_HEIGHT // MIDDLE
         self.color = color
 
     def update(self, y):
@@ -188,64 +231,91 @@ class Button():
 		else:
 			self.text = self.font.render(self.text_input, True, self.base_color)
 
-def main_menu(): #TODO - Hacer que esto se cierre una vez que los jugadores apretan Q, sino da errores al salir uno de los jugadores.
+def draw_menu_text():
+   
+    MENU_TEXT = pygame.font.Font("Tiny5-regular.ttf", MENU_TEXT_SIZE).render("PONG", True, "#b68f40")
+    MENU_RECT = MENU_TEXT.get_rect(center=(MENU_TEXT_X, MENU_TEXT_Y))
+    screen.blit(MENU_TEXT, MENU_RECT)
+
+def create_buttons():
+   
+    PLAY__LAN_BUTTON = Button(None, pos=(PLAY_LAN_BUTTON_X, PLAY_LAN_BUTTON_Y), 
+                        text_input="JUGAR EN LAN", font=pygame.font.Font("Tiny5-regular.ttf", FONT_SIZE), base_color="#d7fcd4", hovering_color="White")
+    QUIT_BUTTON = Button(None, pos=(QUIT_BUTTON_X, QUIT_BUTTON_Y), 
+                        text_input="QUIT", font=pygame.font.Font("Tiny5-regular.ttf", FONT_SIZE), base_color="#d7fcd4", hovering_color="White")
+    return [PLAY__LAN_BUTTON, QUIT_BUTTON]
+
+def update_buttons(buttons, mouse_pos):
+    for button in buttons:
+        button.changeColor(mouse_pos)
+        button.update(screen)
+
+def handle_events(buttons, mouse_pos):
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if buttons[0].checkForInput(mouse_pos):
+                play_lan()
+            if buttons[1].checkForInput(mouse_pos):
+                pygame.quit()
+                sys.exit()
+
+def main_menu(): 
     while True:
         screen.fill(BLACK)
-        MENU_MOUSE_POS = pygame.mouse.get_pos()
+        mouse_pos = pygame.mouse.get_pos()
 
-        MENU_TEXT = pygame.font.Font("Tiny5-regular.ttf",100).render("PONG", True, "#b68f40")
-        MENU_RECT = MENU_TEXT.get_rect(center=(400, 100))
-
-        PLAY__LAN_BUTTON = Button(None, pos=(400, 250), 
-                            text_input="JUGAR EN LAN", font=pygame.font.Font("Tiny5-regular.ttf",75), base_color="#d7fcd4", hovering_color="White")
-        QUIT_BUTTON = Button(None, pos=(400, 400), 
-                            text_input="QUIT", font=pygame.font.Font("Tiny5-regular.ttf",75), base_color="#d7fcd4", hovering_color="White")
-
-        screen.blit(MENU_TEXT, MENU_RECT)
-
-        for button in [PLAY__LAN_BUTTON, QUIT_BUTTON]:
-            button.changeColor(MENU_MOUSE_POS)
-            button.update(screen)
-        
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                #pygame.sys.exit()
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                if PLAY__LAN_BUTTON.checkForInput(MENU_MOUSE_POS):
-                    play_lan()
-                if QUIT_BUTTON.checkForInput(MENU_MOUSE_POS):
-                   pygame.quit()
+        draw_menu_text()
+        buttons = create_buttons()
+        update_buttons(buttons, mouse_pos)
+        handle_events(buttons, mouse_pos)
         
         pygame.display.update()
 
+
+def show_loading_screen():
+    screen.fill(BLACK)
+    
+    LOADING_TEXT = pygame.font.Font("Tiny5-regular.ttf", LOADING_TEXT_FONT_SIZE).render("Loading...", True, "#b68f40")
+    LOADING_RECT = LOADING_TEXT.get_rect(center=(LOADING_TEXT_X, LOADING_TEXT_Y))
+    screen.blit(LOADING_TEXT, LOADING_RECT)
+    pygame.display.update()
+
+def show_fail_screen():
+    screen.fill(BLACK)
+    
+    FAIL_TEXT = pygame.font.Font("Tiny5-regular.ttf", FAIL_TEXT_FONT_SIZE).render("No se pudo establecer la conexion con el servidor", True, "#FF0000")
+    FAIL_RECT = FAIL_TEXT.get_rect(center=(FAIL_TEXT_X, FAIL_TEXT_Y))
+    screen.blit(FAIL_TEXT, FAIL_RECT)
+    pygame.display.update()
+
+def try_connect():
+    return Paddle_Client(IP, PORT)
 
 def play_lan():
     loading = True
     intentos = 0
     client = None
+
     while loading:
-        try : 
-            screen.fill(BLACK)
-            LOADING_TEXT = pygame.font.Font("Tiny5-regular.ttf",100).render("Loading...", True, "#b68f40")
-            LOADING_RECT = LOADING_TEXT.get_rect(center=(400, 300))
-            screen.blit(LOADING_TEXT, LOADING_RECT)
-            pygame.display.update()
-            client = Paddle_Client("127.0.0.1", 5555)
+        try:
+            show_loading_screen()
+            client = try_connect()
             loading = False
         except:
-            intentos+=1
-            if intentos > 2 :
-                screen.fill(BLACK)
-                FAIL_TEXT = pygame.font.Font("Tiny5-regular.ttf",25).render("No se pudo establecer la conexion con el servidor", True, "#FF0000")
-                FAIL_RECT = FAIL_TEXT.get_rect(center=(400, 300))
-                screen.blit(FAIL_TEXT, FAIL_RECT)
-                pygame.display.update()
+            intentos += 1
+            if intentos > FAIL:
+                show_fail_screen()
                 break
             print("try again")
-    if client:         
-        screen.fill(BLACK)     
+
+    if client:
+        screen.fill(BLACK)
         pygame.display.update()
         client.start_game()
+
+
 if __name__ == "__main__":
     main_menu()
--- servidor.py
+++ servidor.py
@@ -4,10 +4,9 @@ import pygame
 import random
 import time
 
-# Dimensiones de la pantalla
+
 WIDTH, HEIGHT = 800, 600
 
-#parámetros de los elementos
 BALL_RADIUS = 10
 BALL_SPEED_X = 5  # Velocidad horizontal de la pelota
 BALL_SPEED_Y = 5 # Velocidad vertical de la pelota
@@ -17,29 +16,35 @@ PADDLE_LEFT_START_WIDTH = 20
 PADDLE_RIGHT_END_WIDTH = WIDTH - PADDLE_LEFT_START_WIDTH - PADDLE_WIDTH
 PADDLE_SPEED = 10
 MAX_SCORE = 2
-
+CHANGE_DIRECTION = -1
+NONE = -1
+ZERO = 0
+PLAYER_ONE = 0
+PLAYER_TWO = 1
+LISTEN = 2
+PORT = 5555
+IP = '0.0.0.0'
 PAUSE_TIME = 5
-
-#colores
+DELAY = 30
+BUFFER = 1024
+MIDDLE = 2
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 
 class Server:
     def __init__(self):
-        #self.start_game()
         return
 
     def start_game(self): #recibe la conexión e instancia un game
         server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        server.bind(('0.0.0.0', 5555))
-        server.listen(2)
+        server.bind((IP, PORT))
+        server.listen()
 
         print("Esperando conexiones...")
         conn1, addr1 = server.accept()
         print(f"Jugador 1 conectado: {addr1}")
         conn2, addr2 = server.accept()
         print(f"Jugador 2 conectado: {addr2}")
-
 
         self.game = Game(conn1, conn2)
         self.game.start_game()
@@ -53,27 +58,27 @@ class Game: #game se instancia con los threads de los jugadores que le voy a pas
         self.ball = Ball()
         self.pad_left = Paddle(PADDLE_LEFT_START_WIDTH)
         self.pad_right = Paddle(PADDLE_RIGHT_END_WIDTH)
-        self.score = [0,0]
-        self.running = True #capaz no hace falta
+        self.score = [ZERO, ZERO]
+        self.running = True 
         self.conn1 = conn1
         self.conn2 = conn2
         self.threads = []
-        self.winner = -1
+        self.winner = NONE
 
     def start_game(self):
-        threading.Thread(target=self.update_client, args=(self.conn1, 0)).start()
-        threading.Thread(target=self.update_client, args=(self.conn2, 1)).start()
+        threading.Thread(target=self.update_client, args=(self.conn1, PLAYER_ONE)).start()
+        threading.Thread(target=self.update_client, args=(self.conn2, PLAYER_TWO)).start()
         threading.Thread(target=self.update_ball).start()
         threading.Thread(target=self.send_game_state, args=(self.conn1,)).start()
         threading.Thread(target=self.send_game_state, args=(self.conn2,)).start()
         return
 
     def reset_game(self):
-        self.score = [0,0]
+        self.score = [ZERO,ZERO]
         self.ball.reset()
         self.pad_left.reset(PADDLE_LEFT_START_WIDTH)
         self.pad_right.reset(PADDLE_RIGHT_END_WIDTH)
-        self.winner=-1
+        self.winner= NONE
 
     def end_game(self):
         self.running = False            
@@ -81,10 +86,9 @@ class Game: #game se instancia con los threads de los jugadores que le voy a pas
     def send_game_state(self, conn):
         while self.running:
             try:
-                game_state = f"{self.pad_left.y},{self.pad_right.y},{self.ball.x},{self.ball.y},{self.score[0]},{self.score[1]},{self.winner}"
-                #print(game_state)
+                game_state = f"{self.pad_left.y},{self.pad_right.y},{self.ball.x},{self.ball.y},{self.score[PLAYER_ONE]},{self.score[PLAYER_TWO]},{self.winner}"
                 conn.sendall(str.encode(game_state))
-                pygame.time.delay(30)  # Ajustar el retraso para la frecuencia de actualización
+                pygame.time.delay(DELAY)  
             except:
                 return
     
@@ -93,54 +97,55 @@ class Game: #game se instancia con los threads de los jugadores que le voy a pas
             self.check_collisions()
             self.check_points()
             self.ball.move()
-            pygame.time.delay(30)
+            pygame.time.delay(DELAY)
     
     def check_collisions(self):
-        # Rebotar en las paletas
-        if self.ball.dx < 0 and self.pad_left.y < self.ball.y < self.pad_left.y + PADDLE_HEIGHT and self.ball.x <= PADDLE_LEFT_START_WIDTH + PADDLE_WIDTH + BALL_RADIUS:
+        if self.ball.dx < ZERO and self.pad_left.y < self.ball.y < self.pad_left.y + PADDLE_HEIGHT and self.ball.x <= PADDLE_LEFT_START_WIDTH + PADDLE_WIDTH + BALL_RADIUS:
             self.ball.ball_speed_modifier += BALL_ACCELERATION
-            self.ball.dx *= -1 * self.ball.ball_speed_modifier
+            self.ball.dx *= CHANGE_DIRECTION * self.ball.ball_speed_modifier
             self.ball.dx = round(self.ball.dx)
-        elif self.ball.dx > 0 and self.pad_right.y < self.ball.y < self.pad_right.y + PADDLE_HEIGHT and self.ball.x >= PADDLE_RIGHT_END_WIDTH - BALL_RADIUS:
+        elif self.ball.dx > ZERO and self.pad_right.y < self.ball.y < self.pad_right.y + PADDLE_HEIGHT and self.ball.x >= PADDLE_RIGHT_END_WIDTH - BALL_RADIUS:
             self.ball.ball_speed_modifier += BALL_ACCELERATION
-            self.ball.dx *= -1 * self.ball.ball_speed_modifier
+            self.ball.dx *= CHANGE_DIRECTION * self.ball.ball_speed_modifier
             self.ball.dx = round(self.ball.dx)
         
     def check_points(self):
-        # Verificar si la pelota ha salido por los lados
-        if self.ball.x < 0:
-            self.score[1] += 1
-            if self.score[1] == MAX_SCORE:
-                self.winner = 0
+        if self.ball.x < ZERO:
+            self.score[PLAYER_TWO] += 1
+            if self.score[PLAYER_TWO] == MAX_SCORE:
+                self.winner = PLAYER_ONE
                 time.sleep(PAUSE_TIME)
                 self.reset_game()
             else:
                 self.ball.reset()
+              
         elif self.ball.x > WIDTH:
-            self.score[0] += 1
-            if self.score[0] == MAX_SCORE:
-                self.winner = 1
+            self.score[PLAYER_ONE] += 1
+            if self.score[PLAYER_ONE] == MAX_SCORE:
+                self.winner = PLAYER_TWO
                 time.sleep(PAUSE_TIME)
                 self.reset_game()
             else:
                 self.ball.reset()
+             
+               
 
     def update_client(self, conn, player):
         conn.send(str.encode(f"{player}"))
         while self.running:
             try:
-                data = conn.recv(1024).decode()
+                data = conn.recv(BUFFER).decode()
                 if data == 'UP':
-                    if player == 0:
+                    if player == PLAYER_ONE:
                         self.pad_left.move_up()
                     else:
                         self.pad_right.move_up()
                 elif data == 'DOWN':
-                    if player == 0:
+                    if player == PLAYER_ONE:
                         self.pad_left.move_down()
                     else:
                         self.pad_right.move_down()
-                elif data == 'QUIT': #AJUSTAR ESTO PORQUE SI SALE UN CLIENTE EL OTRO TAMBIEN. Esto lo tiene que hacer el game
+                elif data == 'QUIT': 
                     self.end_game()
                     break
             except:
@@ -155,8 +160,8 @@ class Ball:
 
     def reset(self):
         self.ball_speed_modifier = float(1)
-        self.x = WIDTH // 2
-        self.y = HEIGHT // 2
+        self.x = WIDTH // MIDDLE
+        self.y = HEIGHT // MIDDLE
         self.dx = random.choice([-1, 1]) * BALL_SPEED_X
         self.dy = random.choice([-1, 1]) * BALL_SPEED_Y
 
@@ -164,10 +169,8 @@ class Ball:
         self.x += self.dx
         self.y += self.dy
 
-        # Rebotar en las paredes superior e inferior
         if self.y <= BALL_RADIUS or self.y >= HEIGHT - BALL_RADIUS:
-            self.dy *= -1
-
+            self.dy *= CHANGE_DIRECTION
 
 
 class Paddle:
@@ -176,8 +179,8 @@ class Paddle:
 
     def move_up(self):
         self.y -= PADDLE_SPEED
-        if self.y < 0:
-            self.y = 0
+        if self.y < ZERO:
+            self.y = ZERO
 
     def move_down(self):
         self.y += PADDLE_SPEED
@@ -186,7 +189,7 @@ class Paddle:
     
     def reset(self, x):
         self.x = x
-        self.y = HEIGHT // 2 - PADDLE_HEIGHT // 2
+        self.y = HEIGHT // MIDDLE - PADDLE_HEIGHT // MIDDLE
 
 
 def main():
